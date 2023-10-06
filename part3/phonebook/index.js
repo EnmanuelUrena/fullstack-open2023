e935879e99f16ec168d66fde1cbeff475ace0450
enmanuelurena <enmanuelurenav@gmail.com>
2023-10-06 10:25:21 -0400

    Backend Deploy 3.10

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors')
 
 const app = express();
 
@@ -34,7 +35,7 @@ function generateRandomIds() {
 
 morgan.token('body', (req, res) => req.method === 'POST' ? JSON.stringify(req.body) : "")
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
+app.use(cors())
 app.use(express.json())
 
 app.get("/", (req, res) => {
